4aad5f30e88e883ab44cfd00e505493fdc06cace
Anzhalika Turlak <293852647@mail.ru>
2023-07-20 16:37:05 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const count = Math.round(str.length / 2);
+  const brackets = []
+  let new_str = str
+
+  for (let elem of bracketsConfig) {
+    brackets.push(elem.join(''));
+  }
+
+  for (let i = 0; i <= count; i++) {
+    if (new_str.length > 1 && new_str.length !== 0) {
+      for (let bracet of brackets) {
+        new_str = new_str.replaceAll(bracet, '');
+      }
+    } else {
+      break;
+    }
+  }
+
+  if (new_str.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
+
